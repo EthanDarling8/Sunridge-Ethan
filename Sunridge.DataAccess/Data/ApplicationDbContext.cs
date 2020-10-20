bf48726c97d68ec0a05f7d32382cb8376c9d8230
Carbon-Fourteen <kylerbourgeous1@mail.weber.edu>
2020-10-20 11:31:58 -0600

    Added projects and packages. Setup database connection. NOT COMPLETE AT ALL. Needs a lot of initial setup still.

---
+++ Sunridge.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Sunridge.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sunridge.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // **** DbSets go here ****
+
+        //public DbSet<Category> Category { get; set; }
+       
+    }
+}
--- Sunridge/Startup.cs
+++ Sunridge/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sunridge.DataAccess.Data;
 
 namespace Sunridge
 {
@@ -23,7 +26,18 @@ namespace Sunridge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages();           
+
+            //database connection
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContext"),
+                sqlServerOptions => sqlServerOptions.MigrationsAssembly("Sunridge.DataAccess")
+                ));
+
+            //identity framework
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

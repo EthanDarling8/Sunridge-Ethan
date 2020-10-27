4315f9d705b9dc577035be1723c0283ce2fa7347
braydenperry <braydenperry1@mail.weber.edu>
2020-10-27 11:07:49 -0600

    Identity scaffolding

--- Sunridge.Models/Models/ApplicationUser.cs
+++ Sunridge.Models/Models/ApplicationUser.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Sunridge.Models.Models
 {
-    public class ApplicationUser /* : IdentityUser */
+    public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
 

14d6467c857ad60bbb93353827d0136618335dae
EthanDarling8 <edarling90@gmail.com>
2020-10-30 15:00:43 -0600

    Board member page cards

--- Sunridge/Pages/Home/BoardMembers/Index.cshtml.cs
+++ Sunridge/Pages/Home/BoardMembers/Index.cshtml.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sunridge.DataAccess.Data;
+using Sunridge.DataAccess.Data.Repository.IRepository;
+using Sunridge.Models;
 
 namespace Sunridge.Pages.Home.BoardMembers {
     public class Index : PageModel {
+        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Index(ApplicationDbContext db, IUnitOfWork unitOfWork) {
+            _db = db;
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public List<ApplicationUser> UserList { get; set; }
+        
         public void OnGet() {
+            UserList = _unitOfWork.ApplicationUser.GetAll().ToList();
             
         }
     }

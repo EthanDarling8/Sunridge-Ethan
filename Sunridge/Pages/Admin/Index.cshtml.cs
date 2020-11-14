f35215e7c386b5e84ee7308d96bc5dff5cf78d55
braydenperry <braydenperry1@mail.weber.edu>
2020-11-14 16:09:07 -0700

    Controller, Page setup

---
+++ Sunridge/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sunridge.DataAccess.Data.Repository.IRepository;
+
+namespace Sunridge.Pages.Admin
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

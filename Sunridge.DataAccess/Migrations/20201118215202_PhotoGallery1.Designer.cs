96d9da6a96e2cd5f4d07992579a2d3209e59f272
Carbon-Fourteen <kylerbourgeous1@mail.weber.edu>
2020-11-18 14:52:50 -0700

    Added migration and updated database to implement PhotoGallery model updates.

--- Sunridge.DataAccess/Migrations/20201118215202_PhotoGallery1.Designer.cs
+++ Sunridge.DataAccess/Migrations/20201118215202_PhotoGallery1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sunridge.DataAccess.Data;
 
 namespace Sunridge.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201118215202_PhotoGallery1")]
+    partial class PhotoGallery1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

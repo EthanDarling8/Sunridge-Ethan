f2a468f44353ec6c97d7026b00f1200a15b285c1
Alden Park <aldenpark@gmail.com>
2020-11-22 21:20:55 -0700

    Added id for the list view

--- Sunridge.Models/Models/ViewModels/LotOwnerInvVM.cs
+++ Sunridge.Models/Models/ViewModels/LotOwnerInvVM.cs
@@ -6,6 +6,7 @@ namespace Sunridge.Models.ViewModels
     public class LotOwnerInvVM
     {
         // The Fields needed to display the Lot Index Page.
+        public int id { get; set; }
         public string LotNumber { get; set; }
         public string Address { get; set; }
         public string Lot_Owner { get; set; }

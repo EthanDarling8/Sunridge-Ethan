c712396e1748e84ea0ba20378eb5c755a704e407
Jacob Lillywhite <jacoblillywhite@mail.weber.edu>
2020-11-30 20:24:20 -0700

    updated home lost item controller

--- Sunridge/Controllers/HomeLostItemController.cs
+++ Sunridge/Controllers/HomeLostItemController.cs
@@ -34,6 +34,33 @@ namespace Sunridge.Controllers
             // Return Lost Items
             return Json(new { data = _unitOfWork.LostItem.GetAll(li => li.Active == true) }); // If the User is an Owner or General Public they only see active listings.
         }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var objFromDb = _unitOfWork.LostItem.GetFirstOrDefault(u => u.Id == id);
+                if (objFromDb == null)
+                {
+                    return Json(new { success = false, message = "Error while deleting" });
+                }
+
+                //physically remove image if it exists
+                var imagePath = Path.Combine(_hostingEnvironment.WebRootPath, objFromDb.Image.Trim('\\'));
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                _unitOfWork.LostItem.Remove(objFromDb);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            return Json(new { success = true, message = "Delete succcessful" });
+        }
     }
 
 }

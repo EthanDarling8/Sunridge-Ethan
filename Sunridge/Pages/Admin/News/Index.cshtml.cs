10367dbc8e943db0ea6431e3aa702c4ea8780288
Alden Park <aldenpark@gmail.com>
2020-12-04 15:19:30 -0700

    Added security to news and banners

--- Sunridge/Pages/Admin/News/Index.cshtml.cs
+++ Sunridge/Pages/Admin/News/Index.cshtml.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sunridge.DataAccess.Data.Repository.IRepository;
+using Sunridge.Utility;
 
 namespace Sunridge.Pages.Admin.News
 {
+    [Authorize(Roles = SD.AdministratorRole)]
     public class IndexModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
